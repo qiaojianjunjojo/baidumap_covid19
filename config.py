b3aea96999f600980f92d66cbfc7e2b92ba85b08
JIANJUN.QIAO <jianjun.qiao@innolux.com>
2021-07-17 10:46:08 +0800

    update some description

--- config.py
+++ config.py
@@ -1,3 +1,2 @@
-# DB = {'usrName':"THAIPL0", 'password':"THAIPL000", 'IP':"00.000.0.000", 'port':0000, 'dbName':"thalw"} #測試環境
-# DB = {'usrName':"PHSUMDA0", 'password':"PHSUMDA0000", 'IP':"00.000.000.00", 'port':0000, 'dbName':"PHARS"} #正式環境
-DB = {'usrName':"PHSUMDA0", 'password':"PHSUMDA0000", 'IP':"00.000.000.00", 'port':0000, 'dbName':"PHARS"} #正式環境
+
+DB = {'usrName':"PHSUMDA0", 'password':"PHSUMDA0000", 'IP':"00.000.000.00", 'port':0000, 'dbName':"PHARS"}
